f56c44cb732604dd80170518b83fe626701ee34f
xingyu <xingyu4j@vip.qq.com>
2023-01-30 10:24:29 +0800

    适配springdoc

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/admin/notify/vo/message/NotifyMessageMyPageReqVO.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/controller/admin/notify/vo/message/NotifyMessageMyPageReqVO.java
@@ -1,4 +1,4 @@
-package cn.iocoder.yudao.module.system.controller.admin.tenant.vo.packages;
+package cn.iocoder.yudao.module.system.controller.admin.notify.vo.message;
 
 import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,22 +11,17 @@ import java.time.LocalDateTime;
 
 import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
-@Schema(description = "管理后台 - 租户套餐分页 Request VO")
+@Schema(description = "管理后台 - 站内信分页 Request VO")
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class TenantPackagePageReqVO extends PageParam {
+public class NotifyMessageMyPageReqVO extends PageParam {
 
-    @Schema(description = "套餐名", example = "VIP")
-    private String name;
+    @Schema(description = "是否已读", example = "true")
+    private Boolean readStatus;
 
-    @Schema(description = "状态", example = "1")
-    private Integer status;
-
-    @Schema(description = "备注", example = "好")
-    private String remark;
-
-    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     @Schema(description = "创建时间")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private LocalDateTime[] createTime;
+
 }

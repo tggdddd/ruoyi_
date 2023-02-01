40d0f03e97f1cc37d5c811d12201f0a0e2d476a4
xingyu <xingyu4j@vip.qq.com>
2023-02-01 16:21:29 +0800

    feat: add Logger

--- yudao-ui-admin-vue3/src/main.ts
+++ yudao-ui-admin-vue3/src/main.ts
@@ -13,14 +13,11 @@ import { setupStore } from '@/store'
 // 全局组件
 import { setupGlobCom } from '@/components'
 
-// 引入 element-plus
+// 引入element-plus
 import { setupElementPlus } from '@/plugins/elementPlus'
 
-// 引入 vxe-table
+// 引入vxe-table
 import { setupVxeTable } from '@/plugins/vxeTable'
-
-// 引入 form-create
-import { setupFormCreate } from '@/plugins/formCreate'
 
 // 引入全局样式
 import '@/styles/index.scss'
@@ -42,19 +39,9 @@ import './permission'
 
 import { isDevMode } from '@/utils/env'
 
-import { MyPD } from '@/components/bpmnProcessDesigner/package/index.js'
-import '@/components/bpmnProcessDesigner/package/theme/index.scss'
-import 'bpmn-js/dist/assets/diagram-js.css'
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+import Logger from '@/utils/logger'
 
-import hljs from 'highlight.js' //导入代码高亮文件
-import 'highlight.js/styles/github.css' //导入代码高亮样式  新版
-
-// 本地开发模式 全局引入 element-plus 样式，加快第一次进入速度
-if (isDevMode()) {
-  console.info(isDevMode())
+if (isDevMode() == true) {
   import('element-plus/dist/index.css')
 }
 
@@ -62,17 +49,7 @@ if (isDevMode()) {
 const setupAll = async () => {
   const app = createApp(App)
 
-  //自定义一个代码高亮指令
-  app.directive('highlight', function (el) {
-    const blocks = el.querySelectorAll('code')
-    blocks.forEach((block: any) => {
-      hljs.highlightElement(block)
-    })
-  })
-
   await setupI18n(app)
-
-  MyPD(app)
 
   setupStore(app)
 
@@ -81,8 +58,6 @@ const setupAll = async () => {
   setupElementPlus(app)
 
   setupVxeTable(app)
-
-  setupFormCreate(app)
 
   setupRouter(app)
 
@@ -94,3 +69,5 @@ const setupAll = async () => {
 }
 
 setupAll()
+
+Logger.prettyPrimary(`欢迎使用`, import.meta.env.VITE_APP_TITLE)

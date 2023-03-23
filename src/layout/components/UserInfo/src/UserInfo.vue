d95df3b9e4f570b9197162a12fd59e20d9a50186
肖润杰 <15014586591@139.com>
2023-03-23 13:44:26 +0800

    完成三方登录-企业微信，增加测试页面

--- src/layout/components/UserInfo/src/UserInfo.vue
+++ src/layout/components/UserInfo/src/UserInfo.vue
@@ -43,9 +43,9 @@ const loginOut = () => {
 const toProfile = async () => {
   push('/user/profile')
 }
-const toDocument = () => {
-  window.open('https://doc.iocoder.cn/')
-}
+// const toDocument = () => {
+//   window.open('https://doc.iocoder.cn/')
+// }
 </script>
 
 <template>
@@ -62,10 +62,10 @@ const toDocument = () => {
           <Icon icon="ep:tools" />
           <div @click="toProfile">{{ t('common.profile') }}</div>
         </ElDropdownItem>
-        <ElDropdownItem>
-          <Icon icon="ep:menu" />
-          <div @click="toDocument">{{ t('common.document') }}</div>
-        </ElDropdownItem>
+        <!--        <ElDropdownItem>-->
+        <!--          <Icon icon="ep:menu" />-->
+        <!--          <div @click="toDocument">{{ t('common.document') }}</div>-->
+        <!--        </ElDropdownItem>-->
         <ElDropdownItem divided @click="loginOut">
           <Icon icon="ep:switch-button" />
           <div>{{ t('common.loginOut') }}</div>
